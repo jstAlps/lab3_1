d83d6601f45d623920b6385248d559a419c73f33
jstAlps <126391714+jstAlps@users.noreply.github.com>
2023-04-12 16:49:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 
 // Функція для додавання нової задачі до списку taskList
 function addTask(event) {
-  event.preventDefault();
-  if (taskInput.value) { //перевірка щоб не заповняти список пустими строками
-    const taskItem = document.createElement('li'); //створення елементу li
-    const taskText = document.createElement('p');
-    taskText.innerText = taskInput.value; //текст у li
-    taskItem.appendChild(taskText);
-    taskList.appendChild(taskItem);
-    taskInput.value = '';
-    taskItem.addEventListener('click', completeTask); //обробник , коли натискають на елемент списку
-    const deleteButton = document.createElement('button'); //створення кнопки видалення задачі
-    deleteButton.innerText = 'Видалити';
-    deleteButton.classList.add('delete-button');
-    deleteButton.addEventListener('click', deleteTask); //обробник події , коли натискають на кнопку видалення
-    taskItem.appendChild(deleteButton);
-  }
+    event.preventDefault();
+    if (taskInput.value) { //перевірка щоб не заповняти список пустими строками
+        const taskItem = document.createElement('li'); //створення елементу li
+        const taskText = document.createElement('p');
+        taskText.innerText = taskInput.value; //текст у li
+        taskItem.appendChild(taskText);
+        taskList.appendChild(taskItem);
+        taskInput.value = '';
+        taskItem.addEventListener('click', completeTask); //обробник , коли натискають на елемент списку
+        const deleteButton = document.createElement('button'); //створення кнопки видалення задачі
+        deleteButton.innerHTML = '<img src = "bin1.png">';
+        deleteButton.classList.add('delete-button');
+        deleteButton.addEventListener('click', deleteTask); //обробник події , коли натискають на кнопку видалення
+        taskItem.appendChild(deleteButton);
+    }
 }
 
 // Функція для видалення задачі із li
+
 function deleteTask(event) {
-  const taskItem = event.target.parentElement;
-  taskList.removeChild(taskItem);
+    const button = event.target.closest('.delete-button');
+    const taskItem = button.closest('li');
+    taskItem.remove();
 }
+
 
 // Функція для позначення завдання як виконане
 function completeTask(event) {
-  const taskItem = event.target;
-  taskItem.classList.toggle('done');
+    const taskItem = event.target;
+    taskItem.classList.toggle('done');
 }
 
 // Додавання обробника подій до форми taskForm
